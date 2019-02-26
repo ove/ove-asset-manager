fb3bed929e11f5f42f9db0d4ceae0b25ee2d620c
Ovidiu Serban <serban.ovidiu.m@gmail.com>
2019-02-26 15:21:06 +0000

    The zip worker guesses the new index file

--- workers/zip/__init__.py
+++ workers/zip/__init__.py
@@ -1,9 +1,12 @@
+import glob
 import logging
+import os
 from tempfile import TemporaryDirectory, NamedTemporaryFile
-from typing import Dict, List
+from typing import Dict, List, Union
 from zipfile import ZipFile
 
 from common.entities import OveMeta, WorkerType
+from common.util import append_slash
 from workers.base import BaseWorker
 
 
@@ -26,4 +29,23 @@ class ZipWorker(BaseWorker):
                 ZipFile(zip_file.name).extractall(path=folder)
                 self._file_controller.upload_asset_folder(project_name=project_name, meta=meta, upload_folder=folder, worker_name=self.name)
 
+            meta.index_file = meta.worker_root + self.name + "/" + _guess_index_file(folder)
+            self._file_controller.set_asset_meta(project_name, meta.name, meta)
+
         logging.info("Finished unzipping %s/%s into the storage ...", project_name, meta.name)
+
+
+_INDEX_FILES = {"index.html", "index.htm", "index.js"}
+
+
+def _guess_index_file(folder: str) -> Union[str, None]:
+    result = None
+
+    for filename in glob.iglob(append_slash(folder) + '**/*', recursive=True):
+        if not os.path.islink(filename) and not os.path.ismount(filename) and os.path.isfile(filename):
+            if not result:
+                result = filename[len(folder) + 1:]
+            elif os.path.basename(filename).lower() in _INDEX_FILES:
+                result = filename[len(folder) + 1:]
+
+    return result or ""
