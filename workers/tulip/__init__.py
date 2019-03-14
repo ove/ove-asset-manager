97b6ddb1bfd85923f2d9eb3dd936c92d8b42799e
James Scott-Brown <james@jamesscottbrown.com>
2019-03-14 16:17:27 +0000

    handle options

--- workers/tulip/__init__.py
+++ workers/tulip/__init__.py
@@ -1,9 +1,9 @@
 import logging
 from tulip import tlp
 import os
-from tempfile import TemporaryDirectory, NamedTemporaryFile
+from tempfile import TemporaryDirectory
 from typing import Dict, List
-
+import json
 
 from common.entities import OveMeta, WorkerType
 from workers.base import BaseWorker
@@ -20,12 +20,56 @@ class NetworkWorker(BaseWorker):
     def description(self) -> str:
         return "Applies a layout algorithm to a network using the Tulip framework"
 
-    # TODO: do docs and parameters
     def docs(self) -> str:
-        return "DeepZoomImageWorker.md"
+        return "Tulip.md"
 
     def parameters(self) -> Dict:
-        return {}
+
+        algorithms = tlp.getLayoutAlgorithmPluginsList()
+
+        properties = {
+            "result_name": {
+                "title": "Output filename:",
+                "type": "string",
+                "required": False
+            },
+            "algorithm": {
+                "title": "Layout Algorithm",
+                "type": "string",
+                "enum": algorithms,
+                "required": True
+            }
+        }
+        dependencies = {}
+        options = {}
+
+        for algorithm in algorithms:
+            default_params = tlp.getDefaultPluginParameters(algorithm)
+
+            for field in default_params:
+                if field != 'result':
+                    field_name = algorithm + "_" + field
+
+                    properties[field_name] = {
+                        'type': 'string',
+                        'title': field_name,
+                        'default': str(default_params[field])
+                    }
+
+                    dependencies[field_name] = ['algorithm']
+
+                    options[field_name] = {'dependencies': {'algorithm': algorithm}}
+
+        return {
+            'schema': {
+                'type': 'object',
+                'properties': properties,
+                "dependencies": dependencies
+            },
+            "options": {
+                "fields": options
+            }
+        }
 
     def process(self, project_name: str, meta: OveMeta, options: Dict):
         logging.info("Copying %s/%s into the temp place ...", project_name, meta.name)
@@ -39,30 +83,50 @@ class NetworkWorker(BaseWorker):
                 network_file = os.path.join(folder2, meta.filename)
                 open(network_file, 'a').close()
 
-                self._file_controller.download_asset(project_name=project_name, asset_name=meta.name, filename=meta.file_location, down_filename=network_file)
+                self._file_controller.download_asset(project_name=project_name, asset_name=meta.name,
+                                                     filename=meta.file_location, down_filename=network_file)
 
                 algorithm = options.get('algorithm')
                 if not algorithm:
                     algorithm = "FM^3 (OGDF)"
 
                 params = tlp.getDefaultPluginParameters(algorithm)
-                for param in options.get('layout_params', []):
-                    params[param] = options['layout_params'][param]
+                for param in params:
+                    params[param] = self.convert_param(options.get(param, params[param]))
 
+                logging.info("Performing layout using algorithm %s and options %s ...", algorithm, params)
                 graph = tlp.loadGraph(network_file)
                 graph.applyLayoutAlgorithm(algorithm, params)
 
                 result_name = options.get('result_name')
                 if not result_name:
                     result_name = meta.name.split('.')[0] + '.gml'
-                    tlp.saveGraph(graph, folder + '/' + result_name)
+                tlp.saveGraph(graph, os.path.join(folder, result_name))
 
-                    result_name = meta.name.split('.')[0] + '.svg'
-                    tlp.saveGraph(graph, folder + '/' + result_name)
-                else:
-                    tlp.saveGraph(graph, folder + '/' + result_name)
+                with open(os.path.join(folder, result_name + ".options"), 'w') as fp:
+                    json.dump(options, fp)
 
-                self._file_controller.upload_asset_folder(project_name=project_name, meta=meta, upload_folder=folder, worker_name=self.name)
-        meta.index_file = meta.worker_root + self.name + "/" + os.path.splitext(os.path.basename(meta.filename))[0] + "/" + result_name
+                self._file_controller.upload_asset_folder(project_name=project_name, meta=meta, upload_folder=folder,
+                                                          worker_name=self.name)
+
+        base_name = os.path.splitext(os.path.basename(meta.filename))[0]
+        meta.index_file = os.path.join(meta.worker_root + self.name, base_name, result_name)
+
         self._file_controller.set_asset_meta(project_name, meta.name, meta)
         logging.info("Finished generating %s/%s into the storage ...", project_name, meta.name)
+
+    @staticmethod
+    def convert_param(param):
+        if not isinstance(param, str):
+            return param
+        elif str.lower(param) == 'true':
+            return True
+        elif str.lower(param) == 'false':
+            return False
+        elif str.lower(param) == 'none':
+            return None
+        elif str.isdigit(param):
+            return int(param)
+        elif param.replace('.', '', 1).isdigit():
+            return float(param)
+        return param
