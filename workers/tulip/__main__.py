13dd13264945aa37f7507f8fc665a5f37e12c55a
James Scott-Brown <james@jamesscottbrown.com>
2019-05-02 22:00:49 +0100

    Remove hostnames from tulip worker dev launcher (#87)

--- workers/tulip/__main__.py
+++ workers/tulip/__main__.py
@@ -6,8 +6,7 @@ from workers.base import setup_worker
 # do not use this in production
 # this is a dev only method provided for convenience
 def main():
-    app = setup_worker(worker_class="workers.tulip.NetworkWorker", port=6092, worker_name="worker-tulip",
-                       hostname="146.169.163.121", service_url="http://gdo-appsdev.dsi.ic.ac.uk:6080/api/workers")
+    app = setup_worker(worker_class="workers.tulip.NetworkWorker", port=6092, worker_name="worker-tulip")
     simple_server.make_server('0.0.0.0', 6092, app).serve_forever()  # nosec
 
 
