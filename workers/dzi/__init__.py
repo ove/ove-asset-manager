1f88f10b411cd0754d4625e484787bbe7034058f
Ovidiu Serban <serban.ovidiu.m@gmail.com>
2019-05-28 16:49:17 +0100

    Using .png suffixes for dzi

--- workers/dzi/__init__.py
+++ workers/dzi/__init__.py
@@ -31,7 +31,7 @@ class DeepZoomImageWorker(BaseWorker):
         with TemporaryDirectory() as folder:
             with NamedTemporaryFile() as image_file:
                 self._file_controller.download_asset(project_name=project_name, asset_name=meta.name, filename=filename, down_filename=image_file.name)
-                pyvips.Image.new_from_file(image_file.name).dzsave(folder + "/image")
+                pyvips.Image.new_from_file(image_file.name).dzsave(folder + "/image", suffix=".png")
                 self._file_controller.upload_asset_folder(project_name=project_name, meta=meta, upload_folder=folder, worker_name=self.name)
         meta.index_file = meta.worker_root + self.name + "/" + os.path.splitext(os.path.basename(filename))[0] + "/" + "image.dzi"
         self._file_controller.set_asset_meta(project_name, meta.name, meta)
