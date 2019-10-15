1d30c86d74f680f9f3e96dcab64dbe7ab34b6898
James Scott-Brown <james@jamesscottbrown.com>
2019-10-15 11:58:46 +0100

    Make command descriptions statements of what they will do

--- cli/__main__.py
+++ cli/__main__.py
@@ -19,14 +19,14 @@ def main():
     parser_user_add.add_argument("user", type=str, help="Username to verify")
     parser_user_add.set_defaults(function=user.verify)
 
-    parser_user_add = subparsers_user.add_parser("add", help="User add command")
+    parser_user_add = subparsers_user.add_parser("add", help="Add user")
     parser_user_add.add_argument("user", type=str, help="Username to add")
     parser_user_add.add_argument("--read", dest="read_groups", type=str, nargs="+", default=[], help="Read access groups")
     parser_user_add.add_argument("--write", dest="write_groups", type=str, nargs="+", default=[], help="Write access groups")
     parser_user_add.add_argument('--admin', dest='admin_access', action='store_const', const=True, default=False, help='Admin access')
     parser_user_add.set_defaults(function=user.add)
 
-    parser_user_edit = subparsers_user.add_parser("edit", help="User edit command")
+    parser_user_edit = subparsers_user.add_parser("edit", help="Edit user")
     parser_user_edit.add_argument("user", type=str, help="Username to edit")
     parser_user_edit.add_argument("--password", dest='reset_password', action='store_const', const=True, default=False, help="Password")
 
@@ -41,15 +41,15 @@ def main():
     parser_user_edit.add_argument('--admin', dest='admin_access', type=str, help='Admin access (y or n)')
     parser_user_edit.set_defaults(function=user.edit)
 
-    parser_user_remove = subparsers_user.add_parser("remove", help="User remove command")
+    parser_user_remove = subparsers_user.add_parser("remove", help="Remove user")
     parser_user_remove.add_argument("user", type=str, help="Username to remove")
     parser_user_remove.set_defaults(function=user.remove)
 
-    parser_user_info = subparsers_user.add_parser("info", help="User info command")
+    parser_user_info = subparsers_user.add_parser("info", help="List information about user")
     parser_user_info.add_argument("user", type=str, help="Username to list info about")
     parser_user_info.set_defaults(function=user.info_user)
 
-    parser_user_list = subparsers_user.add_parser("list", help="User list command")
+    parser_user_list = subparsers_user.add_parser("list", help="List users")
     parser_user_list.set_defaults(function=user.list_users)
 
     args = parser.parse_args()
