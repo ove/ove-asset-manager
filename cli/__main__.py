93abbcd643128fc28e142667369152e3111f1113
James Scott-Brown <james@jamesscottbrown.com>
2019-10-15 16:13:06 +0100

    Improve cli (#203)
    * fix copy/paste errors in help text
    
    * Improve help messages by starting with am-cli rather than 'AssetManager CLI'
    
    * Make command descriptions statements of what they will do
    
    * Rename positional argument user -> username
    This avoids confusion with the subcommand name.
    
    * unify list and info subcommands
    
    * add a description to appear in the help
    
    * make help more specific (information -> permissions)

--- cli/__main__.py
+++ cli/__main__.py
@@ -7,7 +7,7 @@ from common.consts import DEFAULT_AUTH_CONFIG
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="AssetManager CLI")
+    parser = argparse.ArgumentParser(prog="am-cli", description="A commandline interface for the OVE Asset Manager.")
     parser.add_argument("--config", default=DEFAULT_AUTH_CONFIG, type=str, help="Auth config file")
 
     subparsers = parser.add_subparsers()
@@ -16,18 +16,18 @@ def main():
     subparsers_user = parser_user.add_subparsers()
 
     parser_user_add = subparsers_user.add_parser("verify", help="Verify user password")
-    parser_user_add.add_argument("user", type=str, help="Username to add")
+    parser_user_add.add_argument("username", type=str, help="Username to verify")
     parser_user_add.set_defaults(function=user.verify)
 
-    parser_user_add = subparsers_user.add_parser("add", help="User add command")
-    parser_user_add.add_argument("user", type=str, help="Username to add")
+    parser_user_add = subparsers_user.add_parser("add", help="Add user")
+    parser_user_add.add_argument("username", type=str, help="Username to add")
     parser_user_add.add_argument("--read", dest="read_groups", type=str, nargs="+", default=[], help="Read access groups")
     parser_user_add.add_argument("--write", dest="write_groups", type=str, nargs="+", default=[], help="Write access groups")
     parser_user_add.add_argument('--admin', dest='admin_access', action='store_const', const=True, default=False, help='Admin access')
     parser_user_add.set_defaults(function=user.add)
 
-    parser_user_edit = subparsers_user.add_parser("edit", help="User edit command")
-    parser_user_edit.add_argument("user", type=str, help="Username to edit")
+    parser_user_edit = subparsers_user.add_parser("edit", help="Edit user")
+    parser_user_edit.add_argument("username", type=str, help="Username to edit")
     parser_user_edit.add_argument("--password", dest='reset_password', action='store_const', const=True, default=False, help="Password")
 
     parser_user_edit_read = parser_user_edit.add_mutually_exclusive_group()
@@ -41,16 +41,13 @@ def main():
     parser_user_edit.add_argument('--admin', dest='admin_access', type=str, help='Admin access (y or n)')
     parser_user_edit.set_defaults(function=user.edit)
 
-    parser_user_remove = subparsers_user.add_parser("remove", help="User remove command")
-    parser_user_remove.add_argument("user", type=str, help="Username to add")
+    parser_user_remove = subparsers_user.add_parser("remove", help="Remove user")
+    parser_user_remove.add_argument("username", type=str, help="Username to remove")
     parser_user_remove.set_defaults(function=user.remove)
 
-    parser_user_info = subparsers_user.add_parser("info", help="User info command")
-    parser_user_info.add_argument("user", type=str, help="Username to add")
+    parser_user_info = subparsers_user.add_parser("info", help="List permissions for specific user, or all users")
+    parser_user_info.add_argument("username", type=str, nargs='?', default='', help="Username to list info about")
     parser_user_info.set_defaults(function=user.info_user)
-
-    parser_user_list = subparsers_user.add_parser("list", help="User list command")
-    parser_user_list.set_defaults(function=user.list_users)
 
     args = parser.parse_args()
 
--- cli/user.py
+++ cli/user.py
@@ -7,8 +7,8 @@ from common.util import to_bool
 
 
 def verify(auth: AuthManager, args: Namespace):
-    pswd = getpass.getpass("[{}] password:".format(args.user))
-    user = auth.auth_user(user=args.user, password=pswd)
+    pswd = getpass.getpass("[{}] password:".format(args.username))
+    user = auth.auth_user(user=args.username, password=pswd)
     if user:
         _format_user(user, multi=False)
     else:
@@ -16,19 +16,19 @@ def verify(auth: AuthManager, args: Namespace):
 
 
 def add(auth: AuthManager, args: Namespace):
-    pswd = getpass.getpass("[{}] password:".format(args.user))
+    pswd = getpass.getpass("[{}] password:".format(args.username))
     if pswd and len(pswd) >= 8:
         setattr(args, "password", pswd)
     else:
         print("[Error] Passwords should be longer than 8 characters")
         return
 
-    _edit(user=UserAccessMeta(user=args.user), auth=auth, args=args, add_user=True)
+    _edit(user=UserAccessMeta(user=args.username), auth=auth, args=args, add_user=True)
 
 
 def edit(auth: AuthManager, args: Namespace):
     if args.reset_password:
-        pswd = getpass.getpass("[{}] password:".format(args.user))
+        pswd = getpass.getpass("[{}] password:".format(args.username))
         if pswd and len(pswd) >= 8:
             setattr(args, "password", pswd)
         else:
@@ -37,7 +37,7 @@ def edit(auth: AuthManager, args: Namespace):
     else:
         setattr(args, "password", None)
 
-    _edit(user=auth.get_user(user=args.user), auth=auth, args=args, add_user=False)
+    _edit(user=auth.get_user(user=args.username), auth=auth, args=args, add_user=False)
 
 
 def _edit(user: UserAccessMeta, auth: AuthManager, args: Namespace, add_user: bool):
@@ -60,7 +60,7 @@ def _edit(user: UserAccessMeta, auth: AuthManager, args: Namespace, add_user: bo
             setattr(user, "read_groups", value)
 
         if auth.edit_user(access=user, password=args.password, hashed=False, add=add_user):
-            _format_user(auth.get_user(user=args.user), multi=False)
+            _format_user(auth.get_user(user=args.username), multi=False)
         elif add_user:
             print("[Error] Could not add user")
         else:
@@ -70,19 +70,20 @@ def _edit(user: UserAccessMeta, auth: AuthManager, args: Namespace, add_user: bo
 
 
 def remove(auth: AuthManager, args: Namespace):
-    if auth.remove_user(user=args.user):
+    if auth.remove_user(user=args.username):
         print("User removed successfully")
     else:
         print("[Error] User not found")
 
 
 def info_user(auth: AuthManager, args: Namespace):
-    _format_user(auth.get_user(user=args.user), multi=False)
+    if args.username:
+        users = [auth.get_user(user=args.username)]
+    else:
+        users = auth.get_users()
 
-
-def list_users(auth: AuthManager, _args: Namespace):
     first = True
-    for user in auth.get_users():
+    for user in users:
         _format_user(user, multi=not first)
         first = False
 
